99f0476c2e66eaa1f610275c39eadeef7ef8c687
David Horcajada Rodríguez <davidhorcajada36@gmail.com>
2024-01-22 19:28:02 +0100

    Versión Inicial

---
+++ U3-ORM-HIBERNATE/src/miEmpresa/UseHibernateEnterprise.java
@@ -0,0 +1,8 @@
+package miEmpresa;
+
+public class UseHibernateEnterprise {
+	public static void main(String[] args) {
+	HibernateEnterprise h=new HibernateEnterprise();
+	h.addProduct(8, "raton",40);
+	}
+}
